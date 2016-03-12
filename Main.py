56fdb46a3fef555a4f763178130d97b1bf3a37a0
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2016-03-12 16:31:44 -0500

    Plz work. Routing paths Created.

--- Main.py
+++ Main.py
@@ -3,24 +3,18 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/<user>")
 def homepage():
     return render_template("index1.html")
 
 
-@app.route("/signin")
-def signin():
-    return render_template("signin.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
-
-
-@app.route("/speak")
-def speak():
-    return render_template("speak.html")
+@app.route("/logged-in")
+def logged_in():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
