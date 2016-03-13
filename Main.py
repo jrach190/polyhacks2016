81f55c3d585f3ad7bb7f6bb85fbe1d5d0d58d2b7
Jonathan Rach <jonathanrach@Jonathans-MacBook-Pro-2.local>
2016-03-13 07:27:18 -0400

    Committing working version of signup notifications.

--- Main.py
+++ Main.py
@@ -8,13 +8,14 @@ from twilio.rest import TwilioRestClient
 
 """Set up for Twilio"""
 #define account_sid and auth_token and create a TwilioRestClient instance to utilize twilio API
-account_sid = "AC701857f97365f037c714cc0a68c65bbc"
-auth_token = "c8e1bdf9b7d74153b9f8458a74d85613"
+account_sid = "XXX"
+auth_token = "XXX"
 client = TwilioRestClient(account_sid,auth_token)
 
 #create variable value for phone and variable to track signup message
 phone = ""
 msgval=0
+US = "XXX"
 
 #define initial signup messages to send based on settings clicked in signup
 anomsg = "Thank you for signing up for anorexia management reminders. Scheduled eating reminders will proceed at 8:00AM for breakfast, 10:30AM for a morning snack, 12:00PM for lunch, 3:30PM for an afternoon snack, and 7:00PM for dinner."
@@ -39,26 +40,29 @@ def login():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    global msgval
     msgval = 1
     return render_template("signup.html")
 
 
 @app.route("/signup1")
 def signup1():
+    global msgval
     msgval = 2
     return render_template("signup1.html")
 
 
 @app.route("/signup2")
 def signup2():
+    global msgval
     msgval = 3
     return render_template("signup2.html")
 
 
-@app.route("/index")
+@app.route("/index", methods=['GET','POST'])
 def index():
     phone = request.form['phone']
-    message = client.messages.create(to=("+1"+user), from_= "+14079017873", body=amsg)
+    message = client.messages.create(to=("+1"+phone), from_=US, body=amsg)
 
     return render_template("pages/index.html",phone = request.form['phone'])
 
@@ -66,14 +70,14 @@ def index():
 @app.route("/indexa")
 def indexa():
     phone = request.form['phone']
-    message = client.messages.create(to=("+1"+user), from_= "+14079017873", body=anxmsg)
+    message = client.messages.create(to=("+1"+phone), from_=US, body=anxmsg)
     return render_template("pages/indexa.html",phone = request.form['phone'])
 
 
 @app.route("/indexb")
 def indexb():
     phone = request.form['phone']
-    message = client.messages.create(to=("+1"+user), from_= "+14079017873", body=dmsg)
+    message = client.messages.create(to=("+1"+phone), from_=US, body=dmsg)
     return render_template("pages/indexb.html",phone = request.form['phone'])
 
 
@@ -84,7 +88,17 @@ def logged_in():
 
 @app.route("/profile", methods=['GET', 'POST'])
 def profile():
-    return render_template("profile.html")
+    print msgval
+    phone = request.form['phone']
+    if msgval == 1:
+        message = client.messages.create(to=("+1"+phone), from_=US, body=anomsg)
+    elif msgval == 2:
+        message = client.messages.create(to=("+1"+phone), from_=US, body=anxmsg)
+    elif msgval == 3:
+        message = client.messages.create(to=("+1"+phone), from_=US, body=dmsg)
+    else:
+        message = client.messages.create(to=("+1"+phone), from_=US, body=defmsg)
+    return render_template("profile.html",phone = phone)
 
 
 if __name__ == "__main__":
