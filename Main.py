5c54a2af38daf93e882f829938a257002f9419fe
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2016-03-12 22:27:15 -0500

    22 Cup Street

--- Main.py
+++ Main.py
@@ -12,10 +12,44 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/logged-in")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/signup1")
+def signup1():
+    return render_template("signup1.html")
+
+
+@app.route("/signup2")
+def signup2():
+    return render_template("signup2.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("pages/index.html")
+
+
+@app.route("/indexa")
+def indexa():
+    return render_template("pages/indexa.html")
+
+
+@app.route("/indexb")
+def indexb():
+    return render_template("pages/indexb.html")
+
+
+@app.route("/logged")
 def logged_in():
     return render_template("index.html")
 
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
 
 if __name__ == "__main__":
     app.run()
